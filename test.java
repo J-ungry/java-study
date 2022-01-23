564f73b4d1ab71d522bf043f90c44eda36853601
jungry <seojungrain@gmail.com>
2022-01-23 16:10:02 +0900

    first day

--- test.java
+++ test.java
@@ -1,4 +1,9 @@
 public class test {
-
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //주석 입력하는 방법
+        /*
+        주석 입력하는 방법 2
+         */
+    }
 }
