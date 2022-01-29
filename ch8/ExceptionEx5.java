24dc0add8eeb621bd50a5b1477136d3ac1763d18
jungry <seojungrain@gmail.com>
2022-01-29 23:48:51 +0900

    Day 7th

--- ch8/ExceptionEx5.java
+++ ch8/ExceptionEx5.java
@@ -1,4 +1,14 @@
 package ch8;
 
 public class ExceptionEx5 {
+    public static void main(String[] args) {
+        try{
+            Exception e = new Exception("정구리 짱짱 ^^");
+            throw e;
+        } catch(Exception e){
+            System.out.println("Error msg >> "+e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("Program end");
+    }
 }
